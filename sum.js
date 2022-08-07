96204fdbf13e366055497be19a7adbbf66b3c5f9
amangupta4529 <66002687+amangupta4529@users.noreply.github.com>
2022-08-07 16:39:51 +0530

    testing of sum is done

---
+++ sum.js
@@ -0,0 +1,7 @@
+
+const fun=()=>{
+    const a=5;
+    const b=3;
+console.log(a+b);
+}
+export default fun;
---
+++ testingofsumm.js
@@ -0,0 +1,3 @@
+import fun from "./sum"
+if(fun()==8)console.log(true);
+else console.log(false);
